78cf94a2ff10923ddac634c94c97c5bb239ed9f5
ShinuVasudevan <svasudevan@igniteinsurance.ca>
2020-02-16 20:55:22 -0500

    setup

--- src/app.js
+++ src/app.js
@@ -4,7 +4,9 @@ const hbs = require('hbs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-const app = express()
+const app = express();
+
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -48,14 +50,14 @@ app.get('/weather', (req, res) => {
         })
     }
 
-    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+    geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
         if (error) {
-            return res.send({ error })
+            return res.send({error})
         }
 
         forecast(latitude, longitude, (error, forecastData) => {
             if (error) {
-                return res.send({ error })
+                return res.send({error})
             }
 
             res.send({
@@ -96,6 +98,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port + '.')
 })
